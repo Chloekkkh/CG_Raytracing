fba6dabf59d165c9ffaf758ff14255a858feeb12
Ke Hu <13691311519@163.com>
2024-05-11 22:24:50 +0100

    add data scripts

---
+++ Assets/Scripts/Data/EnvironmentSettings.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+[System.Serializable]
+public struct EnvironmentSettings
+{
+    public bool enabled;
+    public Color groundColour;
+    public Color skyColourHorizon;
+    public Color SkyColour;
+    public float sunFocus;
+    public float sunIntensity;
+}
